2e149f682d2b795dffe52bd7da4ca4824e38f7ad
mj52951 <matija.jelavic@chainsafe.io>
2024-06-10 14:57:05 +0200

    Add browser rendering for widget

--- src/Widget/App.tsx
+++ src/Widget/App.tsx
@@ -2,14 +2,17 @@ import { Environment } from '@buildwithsygma/sygma-sdk-core';
 import { SygmaProtocolReactWidget } from '@buildwithsygma/sygmaprotocol-react-widget/build/WidgetReact';
 import React from 'react';
 import { Container } from '@mui/material';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 function App(){
   return (
-    <Container sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <SygmaProtocolReactWidget
-        environment={Environment.TESTNET}
-      />
-    </Container>
+    <BrowserOnly fallback={<div>Loading...</div>}>
+          {() => { 
+            return <Container sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <SygmaProtocolReactWidget environment={Environment.TESTNET} />
+            </Container>
+          }}
+    </BrowserOnly>
   )
 }
 
