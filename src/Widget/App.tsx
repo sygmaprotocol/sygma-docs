e8f4157f6d3a23e3c4cea3e2442a4908de67a675
mj52951 <matija.jelavic@chainsafe.io>
2024-06-10 15:12:12 +0200

    Update import

--- src/Widget/App.tsx
+++ src/Widget/App.tsx
@@ -1,5 +1,4 @@
 import { Environment } from '@buildwithsygma/sygma-sdk-core';
-import { SygmaProtocolReactWidget } from '@buildwithsygma/sygmaprotocol-react-widget/build/WidgetReact';
 import React from 'react';
 import { Container } from '@mui/material';
 import BrowserOnly from '@docusaurus/BrowserOnly';
@@ -7,11 +6,12 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 function App(){
   return (
     <BrowserOnly fallback={<div>Loading...</div>}>
-          {() => { 
-            return <Container sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <SygmaProtocolReactWidget environment={Environment.TESTNET} />
-            </Container>
-          }}
+        {() => {
+          const {SygmaProtocolReactWidget} = require('@buildwithsygma/sygmaprotocol-react-widget/build/WidgetReact'); 
+          return <Container sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+              <SygmaProtocolReactWidget environment={Environment.TESTNET} />
+              </Container>
+        }}
     </BrowserOnly>
   )
 }
